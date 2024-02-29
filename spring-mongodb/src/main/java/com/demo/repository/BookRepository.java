1bc0c49f0cc860154bae6241818a38e6094bd423
SowndaryaMadhu <sownd0501@gmail.com>
2024-02-29 15:43:47 +0530

    Springboot with mongodb

---
+++ spring-mongodb/src/main/java/com/demo/repository/BookRepository.java
@@ -0,0 +1,11 @@
+package com.demo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import com.demo.model.Book;
+
+public interface BookRepository extends MongoRepository<Book, Integer>{
+
+	
+
+}
